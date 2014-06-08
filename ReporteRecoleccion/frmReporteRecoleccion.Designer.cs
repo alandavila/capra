a687e43564b90cd6a5822251d31cfc32422276fe
alandavila <alandavila@gmail.com>
2014-06-08 18:56:34 -0500

    Created a new form to show bitacoras queried and started the query logic

--- ReporteRecoleccion/frmReporteRecoleccion.Designer.cs
+++ ReporteRecoleccion/frmReporteRecoleccion.Designer.cs
@@ -35,17 +35,20 @@
             this.lblFolio = new System.Windows.Forms.Label();
             this.txtFolio = new System.Windows.Forms.TextBox();
             this.btnObtener = new System.Windows.Forms.Button();
-            this.lvBitacoras = new System.Windows.Forms.ListView();
-            this.colFolio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colNombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colFecha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colTambos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.colTotal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pnlFolio = new System.Windows.Forms.Panel();
+            this.chkFolio = new System.Windows.Forms.CheckBox();
+            this.pnlConditions = new System.Windows.Forms.Panel();
+            this.chkFecha = new System.Windows.Forms.CheckBox();
+            this.cmbEmpresa = new System.Windows.Forms.ComboBox();
+            this.cmbChofer = new System.Windows.Forms.ComboBox();
+            this.pnlFolio.SuspendLayout();
+            this.pnlConditions.SuspendLayout();
             this.SuspendLayout();
             // 
             // dtpFechaInicial
             // 
-            this.dtpFechaInicial.Location = new System.Drawing.Point(28, 40);
+            this.dtpFechaInicial.Enabled = false;
+            this.dtpFechaInicial.Location = new System.Drawing.Point(117, 100);
             this.dtpFechaInicial.Name = "dtpFechaInicial";
             this.dtpFechaInicial.Size = new System.Drawing.Size(200, 20);
             this.dtpFechaInicial.TabIndex = 0;
@@ -53,7 +56,7 @@
             // lblFechaInicial
             // 
             this.lblFechaInicial.AutoSize = true;
-            this.lblFechaInicial.Location = new System.Drawing.Point(32, 15);
+            this.lblFechaInicial.Location = new System.Drawing.Point(114, 75);
             this.lblFechaInicial.Name = "lblFechaInicial";
             this.lblFechaInicial.Size = new System.Drawing.Size(70, 13);
             this.lblFechaInicial.TabIndex = 1;
@@ -62,7 +65,7 @@
             // lblFechaFinal
             // 
             this.lblFechaFinal.AutoSize = true;
-            this.lblFechaFinal.Location = new System.Drawing.Point(261, 15);
+            this.lblFechaFinal.Location = new System.Drawing.Point(350, 75);
             this.lblFechaFinal.Name = "lblFechaFinal";
             this.lblFechaFinal.Size = new System.Drawing.Size(65, 13);
             this.lblFechaFinal.TabIndex = 2;
@@ -70,7 +73,8 @@
             // 
             // dtpFechaFinal
             // 
-            this.dtpFechaFinal.Location = new System.Drawing.Point(264, 40);
+            this.dtpFechaFinal.Enabled = false;
+            this.dtpFechaFinal.Location = new System.Drawing.Point(353, 100);
             this.dtpFechaFinal.Name = "dtpFechaFinal";
             this.dtpFechaFinal.Size = new System.Drawing.Size(200, 20);
             this.dtpFechaFinal.TabIndex = 3;
@@ -78,7 +82,7 @@
             // lblFolio
             // 
             this.lblFolio.AutoSize = true;
-            this.lblFolio.Location = new System.Drawing.Point(514, 15);
+            this.lblFolio.Location = new System.Drawing.Point(190, 15);
             this.lblFolio.Name = "lblFolio";
             this.lblFolio.Size = new System.Drawing.Size(32, 13);
             this.lblFolio.TabIndex = 4;
@@ -86,14 +90,15 @@
             // 
             // txtFolio
             // 
-            this.txtFolio.Location = new System.Drawing.Point(517, 40);
+            this.txtFolio.Enabled = false;
+            this.txtFolio.Location = new System.Drawing.Point(228, 11);
             this.txtFolio.Name = "txtFolio";
             this.txtFolio.Size = new System.Drawing.Size(100, 20);
             this.txtFolio.TabIndex = 5;
             // 
             // btnObtener
             // 
-            this.btnObtener.Location = new System.Drawing.Point(264, 236);
+            this.btnObtener.Location = new System.Drawing.Point(252, 258);
             this.btnObtener.Name = "btnObtener";
             this.btnObtener.Size = new System.Drawing.Size(143, 30);
             this.btnObtener.TabIndex = 6;
@@ -101,62 +106,89 @@
             this.btnObtener.UseVisualStyleBackColor = true;
             this.btnObtener.Click += new System.EventHandler(this.btnObtener_Click);
             // 
-            // lvBitacoras
+            // pnlFolio
             // 
-            this.lvBitacoras.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.colFolio,
-            this.colNombre,
-            this.colFecha,
-            this.colTambos,
-            this.colTotal});
-            this.lvBitacoras.Location = new System.Drawing.Point(28, 86);
-            this.lvBitacoras.Name = "lvBitacoras";
-            this.lvBitacoras.Size = new System.Drawing.Size(650, 123);
-            this.lvBitacoras.TabIndex = 7;
-            this.lvBitacoras.UseCompatibleStateImageBehavior = false;
-            this.lvBitacoras.View = System.Windows.Forms.View.Details;
+            this.pnlFolio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlFolio.Controls.Add(this.chkFolio);
+            this.pnlFolio.Controls.Add(this.lblFolio);
+            this.pnlFolio.Controls.Add(this.txtFolio);
+            this.pnlFolio.Location = new System.Drawing.Point(24, 22);
+            this.pnlFolio.Name = "pnlFolio";
+            this.pnlFolio.Size = new System.Drawing.Size(688, 43);
+            this.pnlFolio.TabIndex = 7;
             // 
-            // colFolio
+            // chkFolio
             // 
-            this.colFolio.Text = "Folio";
+            this.chkFolio.AutoSize = true;
+            this.chkFolio.Location = new System.Drawing.Point(16, 11);
+            this.chkFolio.Name = "chkFolio";
+            this.chkFolio.Size = new System.Drawing.Size(145, 17);
+            this.chkFolio.TabIndex = 6;
+            this.chkFolio.Text = "Buscar bitacoras por folio";
+            this.chkFolio.UseVisualStyleBackColor = true;
+            this.chkFolio.CheckedChanged += new System.EventHandler(this.chkFolio_CheckedChanged);
             // 
-            // colNombre
+            // pnlConditions
             // 
-            this.colNombre.Text = "Empresa";
-            this.colNombre.Width = 148;
+            this.pnlConditions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlConditions.Controls.Add(this.cmbChofer);
+            this.pnlConditions.Controls.Add(this.cmbEmpresa);
+            this.pnlConditions.Controls.Add(this.chkFecha);
+            this.pnlConditions.Controls.Add(this.lblFechaInicial);
+            this.pnlConditions.Controls.Add(this.dtpFechaInicial);
+            this.pnlConditions.Controls.Add(this.lblFechaFinal);
+            this.pnlConditions.Controls.Add(this.dtpFechaFinal);
+            this.pnlConditions.Location = new System.Drawing.Point(24, 83);
+            this.pnlConditions.Name = "pnlConditions";
+            this.pnlConditions.Size = new System.Drawing.Size(688, 147);
+            this.pnlConditions.TabIndex = 8;
             // 
-            // colFecha
+            // chkFecha
             // 
-            this.colFecha.Text = "Fecha";
-            this.colFecha.Width = 138;
+            this.chkFecha.AutoSize = true;
+            this.chkFecha.Location = new System.Drawing.Point(16, 75);
+            this.chkFecha.Name = "chkFecha";
+            this.chkFecha.Size = new System.Drawing.Size(81, 17);
+            this.chkFecha.TabIndex = 4;
+            this.chkFecha.Text = "Usar Fecha";
+            this.chkFecha.UseVisualStyleBackColor = true;
+            this.chkFecha.CheckedChanged += new System.EventHandler(this.chkFecha_CheckedChanged);
             // 
-            // colTambos
+            // cmbEmpresa
             // 
-            this.colTambos.Text = "Tambos";
-            this.colTambos.Width = 101;
+            this.cmbEmpresa.FormattingEnabled = true;
+            this.cmbEmpresa.Location = new System.Drawing.Point(16, 20);
+            this.cmbEmpresa.Name = "cmbEmpresa";
+            this.cmbEmpresa.Size = new System.Drawing.Size(121, 21);
+            this.cmbEmpresa.TabIndex = 5;
+            this.cmbEmpresa.Text = "Empresa";
+            this.cmbEmpresa.SelectedIndexChanged += new System.EventHandler(this.cmbEmpresa_SelectedIndexChanged);
+            this.cmbEmpresa.Leave += new System.EventHandler(this.cmbEmpresa_Leave);
             // 
-            // colTotal
+            // cmbChofer
             // 
-            this.colTotal.Text = "Total";
-            this.colTotal.Width = 112;
+            this.cmbChofer.FormattingEnabled = true;
+            this.cmbChofer.Location = new System.Drawing.Point(193, 20);
+            this.cmbChofer.Name = "cmbChofer";
+            this.cmbChofer.Size = new System.Drawing.Size(121, 21);
+            this.cmbChofer.TabIndex = 6;
+            this.cmbChofer.Text = "Chofer";
             // 
             // frmReporteRecoleccion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 300);
-            this.Controls.Add(this.lvBitacoras);
+            this.Controls.Add(this.pnlConditions);
+            this.Controls.Add(this.pnlFolio);
             this.Controls.Add(this.btnObtener);
-            this.Controls.Add(this.txtFolio);
-            this.Controls.Add(this.lblFolio);
-            this.Controls.Add(this.dtpFechaFinal);
-            this.Controls.Add(this.lblFechaFinal);
-            this.Controls.Add(this.lblFechaInicial);
-            this.Controls.Add(this.dtpFechaInicial);
             this.Name = "frmReporteRecoleccion";
             this.Text = "Reporte Recoleccion";
+            this.pnlFolio.ResumeLayout(false);
+            this.pnlFolio.PerformLayout();
+            this.pnlConditions.ResumeLayout(false);
+            this.pnlConditions.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -169,12 +201,12 @@
         private System.Windows.Forms.Label lblFolio;
         private System.Windows.Forms.TextBox txtFolio;
         private System.Windows.Forms.Button btnObtener;
-        private System.Windows.Forms.ListView lvBitacoras;
-        private System.Windows.Forms.ColumnHeader colFolio;
-        private System.Windows.Forms.ColumnHeader colNombre;
-        private System.Windows.Forms.ColumnHeader colFecha;
-        private System.Windows.Forms.ColumnHeader colTambos;
-        private System.Windows.Forms.ColumnHeader colTotal;
+        private System.Windows.Forms.Panel pnlFolio;
+        private System.Windows.Forms.CheckBox chkFolio;
+        private System.Windows.Forms.Panel pnlConditions;
+        private System.Windows.Forms.ComboBox cmbChofer;
+        private System.Windows.Forms.ComboBox cmbEmpresa;
+        private System.Windows.Forms.CheckBox chkFecha;
     }
 }
 
--- ReporteRecoleccion/frmReporteRecoleccion.cs
+++ ReporteRecoleccion/frmReporteRecoleccion.cs
@@ -7,37 +7,217 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using DatabaseLib;
+using ValidationLib;
 
 namespace ReporteRecoleccion
 {
     public partial class frmReporteRecoleccion : Form
     {
+
+        private int _checkFolio;
+        private int _checkFecha;
+        SortedList<string, Cliente> listaClientes = new SortedList<string, Cliente>();
+        List<Cliente> clientes = new List<Cliente>();
+        List<Chofer>  choferes = new List<Chofer>();
+
         public frmReporteRecoleccion()
         {
             InitializeComponent();
+            _checkFolio = 0;
+            _checkFecha = 0;
+            this.LoadEmpresaComboBox();
         }
 
         private void btnObtener_Click(object sender, EventArgs e)
         {
             List<Bitacora> bitacoras = new List<Bitacora>();
             bitacoras = DatabaseLib.BitacoraDB.GetBitacoras();
-            //if user clicks Obtener twice the list will be filled already
-            lvBitacoras.Items.Clear();
+            List<Bitacora> queriedBitacoras =  new List<Bitacora>();
+            Validator validator = new Validator();
+        
+            if (this.chkFolio.Enabled == true) 
+            {
+                if(this.txtFolio.Text != string.Empty)
+                {
+                    int Numfolio = -1;
+                    if (validator.ValidateNumberInt(this.txtFolio.Text))
+                        Numfolio = int.Parse(this.txtFolio.Text);
+                    else
+                        MessageBox.Show("El numero de folio no es valido.", "Datos incorrectos");
 
-            var bitacorasList = from bitacora in bitacoras select bitacora;
-            string teststr = "";
-            int i = 0;
-            foreach (var bitacora in bitacorasList) {
-                this.lvBitacoras.Items.Add(bitacora.BitacoraID.ToString());
-                this.lvBitacoras.Items[i].SubItems.Add(bitacora.Empresa.Trim());
-                this.lvBitacoras.Items[i].SubItems.Add(bitacora.HoraEntrada.Trim());
-                this.lvBitacoras.Items[i].SubItems.Add(bitacora.NumTambos.ToString().Trim());
-                this.lvBitacoras.Items[i].SubItems.Add(bitacora.Total.ToString().Trim());
-                i += 1;
-                   teststr += bitacora.Empresa.Trim() +" ";
+                    queriedBitacoras = (from bitacora in bitacoras
+                                        where bitacora.BitacoraID == Numfolio
+                                        select bitacora).ToList<Bitacora>();
+                }
+                else
+                {
+                    MessageBox.Show("Favor de especificar un numero de folio","Faltan Datos");
+                }
             }
-            MessageBox.Show(teststr,"Empresas en Base de datos ");
+            else
+            {
+                if (this.chkFecha.Enabled == true)
+                {
+                    if (this.cmbChofer.Text == "Chofer" && this.cmbEmpresa.Text == "Empresa") 
+                    {
+                        queriedBitacoras = (from bitacora in bitacoras
+                                            where (DateTime.Parse(bitacora.HoraEntrada) >= dtpFechaInicial.Value.Date) && ((DateTime.Parse(bitacora.HoraSalida) <= dtpFechaFinal.Value.Date))
+                                            select bitacora).ToList<Bitacora>();
+                    }
+                    else if (this.cmbChofer.Text == "Chofer" && this.cmbEmpresa.Text != "Empresa")
+                    {
+                        queriedBitacoras = (from bitacora in bitacoras
+                                            where (DateTime.Parse(bitacora.HoraEntrada) >= dtpFechaInicial.Value.Date) && ((DateTime.Parse(bitacora.HoraSalida) <= dtpFechaFinal.Value.Date) && this.cmbEmpresa.Text == bitacora.Empresa)
+                                            select bitacora).ToList<Bitacora>();
+                    }
+                    else if (this.cmbChofer.Text != "Chofer" && this.cmbEmpresa.Text == "Empresa")
+                    {
+                        queriedBitacoras = (from bitacora in bitacoras
+                                            where (DateTime.Parse(bitacora.HoraEntrada) >= dtpFechaInicial.Value.Date) && ((DateTime.Parse(bitacora.HoraSalida) <= dtpFechaFinal.Value.Date) && this.cmbChofer.Text == bitacora.Chofer)
+                                            select bitacora).ToList<Bitacora>();
+                    }
+                }
+                else 
+                {
+                    if (this.cmbChofer.Text == "Chofer" && this.cmbEmpresa.Text == "Empresa")
+                    {
+                        MessageBox.Show("Favor de especificar chofer y/o empresa.","Faltan Datos");
+                    }
+                    else if (this.cmbChofer.Text == "Chofer" && this.cmbEmpresa.Text != "Empresa")
+                    {
+                        queriedBitacoras = (from bitacora in bitacoras
+                                            where  this.cmbEmpresa.Text == bitacora.Empresa
+                                            select bitacora).ToList<Bitacora>();
+                    }
+                    else if (this.cmbChofer.Text != "Chofer" && this.cmbEmpresa.Text == "Empresa")
+                    {
+                        queriedBitacoras = (from bitacora in bitacoras
+                                            where this.cmbChofer.Text == bitacora.Chofer
+                                            select bitacora).ToList<Bitacora>();
+                    }
+                }
+            }
+/*
+            var bitacorasList = from bitacora in bitacoras
+                                where bitacora.BitacoraID == Numfolio
+                                select bitacora;
+ */
+            int i = 0;
+            //frmReporteListView.FillReport(someclasshere);
+            frmReporteBitacoras frmReporteListView = new frmReporteBitacoras();
+            this.AddOwnedForm(frmReporteListView);
+            
+            i = 0;
+//            foreach (var bitacora in bitacorasList)
+              foreach (Bitacora bitacora in queriedBitacoras)
+            {
+                frmReporteListView.lvListView.Items.Add(bitacora.BitacoraID.ToString());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Empresa.Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Chofer.Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.HoraEntrada.Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.NumTambos.ToString());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.PrecioUnitario.ToString().Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Subotal.ToString().Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Iva.ToString().Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Total.ToString().Trim());
+                frmReporteListView.lvListView.Items[i].SubItems.Add(bitacora.Observaciones);
+                i += 1;
+            }
+            
+            DialogResult result = frmReporteListView.ShowDialog(this);
+
        
+        }
+
+        private void chkFolio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_checkFolio % 2 == 0)
+            {
+                this.pnlConditions.Enabled = false;
+                this.txtFolio.Enabled = true;
+            }
+            else
+            {
+                this.pnlConditions.Enabled = true;
+                this.LoadEmpresaComboBox();
+                this.txtFolio.Enabled = false;
+            }
+            _checkFolio += 1;
+        }
+
+        //populate Empresa combo box
+        private void LoadEmpresaComboBox()
+        {
+            try
+            {
+                clientes = ClientesDB.GetClients();
+                cmbEmpresa.DataSource = clientes;
+                //tengo que usar un methodo (get) de Cliente
+                //que es el elemento de la "list"
+                cmbEmpresa.DisplayMember = "Nombre";
+                cmbEmpresa.ValueMember = "ClienteID";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+
+            }
+        }
+
+        //populate Empresa combo box
+        private void LoadChoferComboBox()
+        {
+            try
+            {
+
+                choferes = listaClientes[cmbEmpresa.Text.Trim()].choferes;
+                cmbChofer.DataSource = choferes;
+                cmbChofer.DisplayMember = "Nombre";
+                cmbChofer.ValueMember = "ChoferID";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void cmbEmpresa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            try
+            {
+                //get clients from database and fill the list with them
+                clientes = ClientesDB.GetClients();
+                //get a sorted list of clients with key =  name to use in form
+                listaClientes = ClientesDB.GetClientsList();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+
+            }
+        }
+
+        private void cmbEmpresa_Leave(object sender, EventArgs e)
+        {
+            this.LoadChoferComboBox();
+        }
+
+        private void chkFecha_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_checkFecha % 2 == 0)
+            {
+                this.dtpFechaFinal.Enabled = true;
+                this.dtpFechaInicial.Enabled = true;
+            }
+            else
+            {
+                this.dtpFechaFinal.Enabled = false;
+                this.dtpFechaInicial.Enabled = false;
+            }
+            _checkFecha += 1;
         }
     }
 }
